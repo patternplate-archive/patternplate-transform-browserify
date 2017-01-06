0c0d55d24add8ede665d278e51b43c2759778132
Mathis Wiehl <mathis.wiehl@sinnerschrader.com>
2017-01-06 13:02:59 +0100

    test: node_module loading

--- test/test-index.js
+++ test/test-index.js
@@ -3,6 +3,15 @@
 import test from 'ava';
 import factory from '../source';
 import * as mocks from './mocks';
+import {createFakeDir, createFakeModule, createLinkedFakeModule, cleanUpFakes} from './fakes';
+
+test.before('create fake directory', () => {
+	createFakeDir();
+});
+
+test.after.always('clean up fakes', () => {
+	cleanUpFakes();
+});
 
 test('it should export a function as default', t => {
 	const actual = typeof factory;
@@ -40,4 +49,16 @@ test('the resolved object should have a buffer key for buffer files', async t =>
 	const transform = factory(mocks.application);
 	const file = await transform(mocks.emptyBufferFile);
 	t.truthy(Object.prototype.hasOwnProperty.call(file, 'buffer'));
+});
+
+test('load module dependencies', async () => {
+	createFakeModule();
+	const transform = factory(mocks.application);
+	await transform(mocks.fileWithModuleDeps);
+});
+
+test('load linked module dependencies', async () => {
+	createLinkedFakeModule();
+	const transform = factory(mocks.application);
+	await transform(mocks.fileWithLinkedModuleDeps);
 });
